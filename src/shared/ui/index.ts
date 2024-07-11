4aff1fe66dd051dfbc3a0eca18ef75b954c235f3
PerQuis <thetahirpl@gmail.com>
2024-07-11 18:25:10 +0200

    :building_construction: arch(index.ts): Exported two components: Container and GlobalLayout.

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,6 @@
 export * from "./bold/bold";
+export * from "./container/container";
+export * from "./global-layout/global-layout";
 export * from "./paragraph/paragraph";
 export * from "./section/section";
 export * from "./title/title";
