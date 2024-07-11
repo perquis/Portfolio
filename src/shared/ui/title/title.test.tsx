6dfe574abefbf22be83d9751fecefcef7da89a65
PerQuis <thetahirpl@gmail.com>
2024-07-11 12:44:46 +0200

    :white_check_mark: tests(title.test.tsx): Tested rendering a Title component in DOM.

---
+++ src/shared/ui/title/title.test.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+
+import { Title } from "@/shared/ui/title/title";
+
+describe("Title", () => {
+  it("renders a title component with text", () => {
+    const txt = "Hello World!";
+
+    render(<Title level="h2">{txt}</Title>);
+    const title = screen.getByText(txt);
+
+    expect(title).toBeInTheDocument();
+  });
+});
