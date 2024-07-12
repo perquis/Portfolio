fca538ca571f3cfec758ebf7621d26797a266140
PerQuis <thetahirpl@gmail.com>
2024-07-12 16:48:34 +0200

    :fire: prune(tailwind.config.js): Removed unnecessary css styles.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,7 @@ const config: Config = {
   darkMode: ["class", '[data-mode="dark"]'],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
