d760ff9fd46dd9d942ab55b2ba8b4a8526d50175
PerQuis <thetahirpl@gmail.com>
2024-07-13 10:48:24 +0200

    :lipstick: ui(logo.tsx): Updated classes for bolded text at the Logo component.

--- src/shared/ui/logo/logo.tsx
+++ src/shared/ui/logo/logo.tsx
@@ -5,9 +5,9 @@ import { Avatar } from "@/shared/ui";
 
 export const Logo = () => {
   return (
-    <Link href="/" className="flex flex-row gap-5 w-fit">
+    <Link href="/" className="flex flex-row gap-5 w-fit items-center">
       <Avatar rounded="full" size="tiny" src={emptyPlaceholderImage} alt="Damian Werens" />
-      <b>Damian Werens</b>
+      <b className="dark:text-white text-sm">Damian Werens</b>
     </Link>
   );
 };
