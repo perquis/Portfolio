9a5c9d67eb97e6fea01bf653a617deb23803b5cb
PerQuis <thetahirpl@gmail.com>
2024-07-13 15:11:10 +0200

    :recycle: refactor(icon-button.tsx): Introduced fixes to component to improve developer experiences and debugging.

--- src/shared/ui/icon-button/icon-button.tsx
+++ src/shared/ui/icon-button/icon-button.tsx
@@ -12,13 +12,7 @@ interface IIconButton {
 }
 
 export const IconButton: FC<IIconButton & ComponentProps<"button">> = ({ size, icon, className, ...props }) => {
-  const matchedPadding = match(size)
-    .with("small", () => "p-1 rounded-md")
-    .with("medium", () => "p-[6px] rounded-lg")
-    .with("large", () => "p-3 rounded-xl")
-    .exhaustive();
-
-  const matchedSize = match(size)
+  const squareSize = match(size)
     .with("small", () => 16)
     .with("medium", () => 20)
     .with("large", () => 24)
@@ -26,16 +20,35 @@ export const IconButton: FC<IIconButton & ComponentProps<"button">> = ({ size, i
 
   const Icon = icons[icon];
 
+  const viewBox = {
+    width: squareSize,
+    height: squareSize,
+  };
+
+  return (
+    <Button size={size} {...props}>
+      <Icon {...viewBox} />
+    </Button>
+  );
+};
+
+const Button: FC<Pick<IIconButton, "size"> & ComponentProps<"button">> = ({ className, size, children, ...props }) => {
+  const classes = match(size)
+    .with("small", () => "p-1 rounded-md")
+    .with("medium", () => "p-[6px] rounded-lg")
+    .with("large", () => "p-3 rounded-xl")
+    .exhaustive();
+
   return (
     <button
       className={clsx(
         "text-zinc-700 dark:text-zinc-500 hover:text-zinc-950 focus-visible:text-zinc-950 hover:bg-zinc-100 focus-visible:bg-zinc-100 active:opacity-75 dark:hover:bg-zinc-900 dark:focus-visible:bg-zinc-900 ease-in-out transition-colors duration-300 dark:hover:text-zinc-50 dark:focus-visible:text-zinc-50",
-        matchedPadding,
+        classes,
         className,
       )}
       {...props}
     >
-      <Icon width={matchedSize} height={matchedSize} />
+      {children}
     </button>
   );
 };
