b0c15dad322d6ad4aff07c184e7ad971129587be
PerQuis <thetahirpl@gmail.com>
2024-07-13 15:54:22 +0200

    :lipstick: ui(padding): Introduced fixes to padding for the Container and GlobalLayout components.

--- src/shared/ui/container/container.tsx
+++ src/shared/ui/container/container.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import type { ComponentProps, FC } from "react";
 
-export const Container: FC<ComponentProps<"main">> = ({ children, className = "py-32", ...props }) => {
+export const Container: FC<ComponentProps<"main">> = ({ children, className, ...props }) => {
   return (
     <main className={clsx("text-zinc-950 dark:text-white max-w-screen-sm mx-auto w-full", className)} {...props}>
       {children}
--- src/shared/ui/global-layout/global-layout.tsx
+++ src/shared/ui/global-layout/global-layout.tsx
@@ -1,7 +1,7 @@
 import type { ComponentProps, FC } from "react";
 
 export const GlobalLayout: FC<ComponentProps<"div">> = ({ children, ...props }) => (
-  <div className="bg-white dark:bg-zinc-950 w-full" {...props}>
+  <div className="bg-white dark:bg-zinc-950 w-full py-32" {...props}>
     {children}
   </div>
 );
