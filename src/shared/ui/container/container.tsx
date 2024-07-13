aeb2d75abf73d79e23a1919a686e04d8cf236e99
PerQuis <thetahirpl@gmail.com>
2024-07-13 10:50:40 +0200

    :lipstick: ui(container.tsx): Added default value for className props.

--- src/shared/ui/container/container.tsx
+++ src/shared/ui/container/container.tsx
@@ -1,8 +1,9 @@
+import clsx from "clsx";
 import type { ComponentProps, FC } from "react";
 
-export const Container: FC<ComponentProps<"main">> = ({ children, ...props }) => {
+export const Container: FC<ComponentProps<"main">> = ({ children, className = "py-32", ...props }) => {
   return (
-    <main className="text-zinc-950 dark:text-white max-w-screen-sm mx-auto py-32 w-full" {...props}>
+    <main className={clsx("text-zinc-950 dark:text-white max-w-screen-sm mx-auto w-full", className)} {...props}>
       {children}
     </main>
   );
