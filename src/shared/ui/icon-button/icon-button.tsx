62bad9661a3a5ee2e1a6a78d2f2d96ac868698bb
PerQuis <thetahirpl@gmail.com>
2024-07-13 14:02:22 +0200

    :lipstick: ui(icon-button.tsx): Updated a IconButton component.

--- src/shared/ui/icon-button/icon-button.tsx
+++ src/shared/ui/icon-button/icon-button.tsx
@@ -11,7 +11,7 @@ interface IIconButton {
   size: Exclude<Size, "tiny">;
 }
 
-export const IconButton: FC<IIconButton & ComponentProps<"button">> = ({ size, icon, ...props }) => {
+export const IconButton: FC<IIconButton & ComponentProps<"button">> = ({ size, icon, className, ...props }) => {
   const matchedPadding = match(size)
     .with("small", () => "p-1 rounded-md")
     .with("medium", () => "p-[6px] rounded-lg")
@@ -29,8 +29,9 @@ export const IconButton: FC<IIconButton & ComponentProps<"button">> = ({ size, i
   return (
     <button
       className={clsx(
-        "text-zinc-500 hover:text-zinc-950 focus-visible:text-zinc-950 hover:bg-zinc-100 focus-visible:bg-zinc-100 active:opacity-75 dark:hover:bg-zinc-900 dark:focus-visible:bg-zinc-900 ease-in-out transition-colors duration-300 dark:hover:text-zinc-50 dark:focus-visible:text-zinc-50",
+        "text-zinc-700 dark:text-zinc-500 hover:text-zinc-950 focus-visible:text-zinc-950 hover:bg-zinc-100 focus-visible:bg-zinc-100 active:opacity-75 dark:hover:bg-zinc-900 dark:focus-visible:bg-zinc-900 ease-in-out transition-colors duration-300 dark:hover:text-zinc-50 dark:focus-visible:text-zinc-50",
         matchedPadding,
+        className,
       )}
       {...props}
     >
