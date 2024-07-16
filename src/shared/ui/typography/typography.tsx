6697a7e10bad4646c075dbfb1953b72007866d6c
PerQuis <thetahirpl@gmail.com>
2024-07-16 08:16:05 +0200

    :bug: bugfix(typography.tsx): Created the new variable is called inter to resolve problem with next bug.

--- src/shared/ui/typography/typography.tsx
+++ src/shared/ui/typography/typography.tsx
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
 
+const inter = Inter({ weight: ["400", "500", "600", "700"], subsets: ["latin"] });
+
 const fonts = {
-  inter: Inter({ weight: ["400", "500", "600", "700"], subsets: ["latin"] }),
+  inter,
 } as const;
 
 type Family = keyof typeof fonts;
