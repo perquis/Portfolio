b7bd78ea8bda095897d2a8410260722e6401e894
PerQuis <thetahirpl@gmail.com>
2024-07-16 22:47:03 +0200

    :children_crossing: ux(input.tsx): Added an error message to input to provide key informations for user when they make a mistake while filling the form.

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,16 +1,22 @@
 import clsx from "clsx";
 import type { ComponentProps } from "react";
 
-type IInput = ComponentProps<"input">;
+import { Section } from "@/shared/ui";
 
-export default function Input({ className, ...props }: IInput) {
+type ErrorValidation = { error: boolean; message: string | null };
+type IInput = ComponentProps<"input"> & ErrorValidation;
+
+export default function Input({ className, error, message, ...props }: IInput) {
   return (
-    <input
-      className={clsx(
-        "text-sm py-2.5 px-3 rounded-[10px] bg-zinc-100 dark:bg-zinc-900 hover:bg-zinc-200 focus-visible:bg-zinc-200 dark:hover:bg-zinc-800 dark:focus-visible:bg-zinc-800 placeholder:text-zinc-600 dark:placeholder:text-zinc-400 text-zinc-900 dark:text-zinc-100 focus-visible:border focus-visible:border-white dark:focus-visible:border-zinc-700/50",
-        className,
-      )}
-      {...props}
-    />
+    <Section className="gap-1">
+      <input
+        className={clsx(
+          "text-sm py-2.5 px-3 rounded-[10px] bg-zinc-100 dark:bg-zinc-900 hover:bg-zinc-200 focus-visible:bg-zinc-200 dark:hover:bg-zinc-800 dark:focus-visible:bg-zinc-800 placeholder:text-zinc-600 dark:placeholder:text-zinc-400 text-zinc-900 dark:text-zinc-100 focus-visible:border focus-visible:border-white dark:focus-visible:border-zinc-700/50 w-full",
+          className,
+        )}
+        {...props}
+      />
+      {error && <span className="text-sm text-rose-500">{message}</span>}
+    </Section>
   );
 }
