50edc8526459a87ce1de2a1075ac6a639c05a317
PerQuis <thetahirpl@gmail.com>
2024-07-18 20:48:15 +0200

    :iphone: responsive(navigation.tsx): Added padding to this component to handle mobile screen.

--- src/shared/ui/navigation/navigation.tsx
+++ src/shared/ui/navigation/navigation.tsx
@@ -6,8 +6,8 @@ type INavigation = ComponentProps<typeof Breadcrumbs>;
 
 export default function Navigation({ links }: INavigation) {
   return (
-    <nav className="py-2 bg-white/95 dark:bg-zinc-950/95 border-b border-zinc-200 dark:border-zinc-800 fixed top-0 left-0 w-full backdrop-blur-xl">
-      <Container className="">
+    <nav className="py-2 bg-white/95 dark:bg-zinc-950/95 border-b px-5 sm:px-0 border-zinc-200 dark:border-zinc-800 fixed top-0 left-0 w-full backdrop-blur-xl">
+      <Container>
         <Section className="!flex-row justify-between items-center">
           <Logo />
           <Menu links={links} />
