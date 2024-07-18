f6302fa69de02ab0c5ede5caaa65866949daed80
PerQuis <thetahirpl@gmail.com>
2024-07-18 23:02:20 +0200

    :bug: bugfix(divider): Fixed problem with react attribute.

--- src/shared/ui/divider/divider.tsx
+++ src/shared/ui/divider/divider.tsx
@@ -7,7 +7,7 @@ export default function Divider({ className, ...props }: IDivider) {
   return (
     <div className={clsx("text-zinc-200 dark:text-zinc-800", className)} {...props}>
       <svg width="100%" height="1" viewBox="0 0 640 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <line x1="0.5" y1="0.5" x2="639.5" y2="0.5" stroke="currentColor" strokeLinecap="round" stroke-dasharray="8" />
+        <line x1="0.5" y1="0.5" x2="639.5" y2="0.5" stroke="currentColor" strokeLinecap="round" strokeDasharray="8" />
       </svg>
     </div>
   );
