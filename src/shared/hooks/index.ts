f3f7c262e45dcc3ce01eb86aed72f305feef5ae0
PerQuis <thetahirpl@gmail.com>
2024-07-18 08:13:36 +0200

    :sparkles: feature(use-outside-onclick.ts): Created a useOutsideOnClick hook.
    This hook is useful to close any actions when the user click on anywhere but ref component.

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useEventCallback } from "./use-event-callback/use-event-callback";
 export { default as useOpen } from "./use-open/use-open";
+export { default as useOutsideOnClick } from "./use-outside-onclick/use-outside-onclick";
---
+++ src/shared/hooks/use-outside-onclick/use-outside-onclick.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import type { MouseEvent, RefObject } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+import { useEventCallback } from "@/shared/hooks";
+
+type IElement = (Window & typeof globalThis) | Element | Document;
+
+const useOutsideOnClick = (ref: RefObject<HTMLElement>, close: () => void, element: IElement = window) => {
+  const [isOutsideElement, setIsOutsideElement] = useState(false);
+
+  const callback = useCallback(
+    (event: MouseEvent) => {
+      const target = event.target as HTMLElement,
+        tagName = target.tagName?.toLowerCase();
+
+      if (tagName === "button" || tagName === "a") return;
+
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOutsideElement(true);
+      } else {
+        setIsOutsideElement(false);
+      }
+    },
+    [ref],
+  );
+
+  useEffect(() => {
+    if (isOutsideElement) {
+      setIsOutsideElement(false);
+      return close();
+    }
+
+    return;
+  }, [isOutsideElement, close, setIsOutsideElement]);
+
+  useEventCallback({ eventName: "click", callback, element });
+
+  return isOutsideElement;
+};
+
+export default useOutsideOnClick;
