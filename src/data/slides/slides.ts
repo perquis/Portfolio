5fcbef1b151f121c21772924ad11da41338f906b
PerQuis <thetahirpl@gmail.com>
2024-07-18 21:24:25 +0200

    :seedling: seed(slides.ts): Updated image sources and alternative texts.

--- src/data/slides/slides.ts
+++ src/data/slides/slides.ts
@@ -1,15 +1,11 @@
 const slides = [
   {
-    src: "https://images.unsplash.com/photo-1606542758304-820b04394ac2?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "A beautiful image",
+    src: "https://images.unsplash.com/photo-1553628223-5a98a5cf8e81?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Two men standing on subway during daytime.",
   },
   {
-    src: "https://images.unsplash.com/photo-1607796884038-3638822d5ee2?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "A beautiful image",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1574388628724-c8319d449336?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    alt: "A beautiful image",
+    src: "https://images.unsplash.com/photo-1553628223-9d5ef60eb131?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Multicolored building under clear blue sky.",
   },
 ];
 
