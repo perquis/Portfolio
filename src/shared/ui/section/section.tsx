e3a82eeb40a5f9528d7fbfc64c435b3d6d1227e5
PerQuis <thetahirpl@gmail.com>
2024-07-18 08:18:27 +0200

    :lipstick: ui(section.tsx): Introduced forwardRef to this component.

--- src/shared/ui/section/section.tsx
+++ src/shared/ui/section/section.tsx
@@ -1,12 +1,14 @@
 import clsx from "clsx";
-import type { ComponentProps } from "react";
+import { type ComponentProps, forwardRef } from "react";
 
 type ISection = ComponentProps<"section">;
 
-export default function Section({ children, className, ...props }: ISection) {
+const Section = forwardRef<HTMLDivElement, ISection>(function Section({ children, className, ...props }, ref) {
   return (
-    <section className={clsx("flex flex-col", className)} {...props}>
+    <section className={clsx("flex flex-col", className)} {...props} ref={ref}>
       {children}
     </section>
   );
-}
+});
+
+export default Section;
