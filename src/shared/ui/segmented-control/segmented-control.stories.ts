26031e00f581072ad517053ad0469b29e55e036c
PerQuis <thetahirpl@gmail.com>
2024-07-19 23:21:14 +0200

    :lipstick: ui(segmented-control.tsx): Created a SegmentedControl component.

---
+++ src/shared/ui/segmented-control/segmented-control.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { SegmentedControl } from "@/shared/ui";
+
+const meta = {
+  title: "Controls/SegmentedControl",
+  component: SegmentedControl,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The **SegmentedControl** component is a simple component that displays a set of controls in a visually appealing way. It is often used to display a set of options.",
+      },
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof SegmentedControl>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    controls: [
+      {
+        icon: "Next",
+        name: "next.config.mjs",
+      },
+      {
+        icon: "Tsx",
+        name: "page.tsx",
+      },
+    ],
+  },
+};
---
+++ src/shared/ui/segmented-control/segmented-control.tsx
@@ -0,0 +1,50 @@
+import clsx from "clsx";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+import * as stacks from "@/shared/icons/stacks";
+import { Section } from "@/shared/ui";
+
+type IconName = keyof typeof stacks;
+
+interface Control {
+  icon: IconName;
+  name: string;
+}
+
+type ISegmentedControl = Record<"controls", Control[]>;
+
+export default function SegmentedControl({ controls }: ISegmentedControl) {
+  const [selected, setSelected] = useState<string | null>(controls[0].name);
+
+  return (
+    <Section className="relative !flex-row gap-2 p-1 rounded-[10px] w-fit bg-zinc-100 dark:bg-zinc-950 dark:ring-1 dark:ring-zinc-900">
+      {controls.map(({ name, icon }, index) => {
+        const Icon = stacks[icon];
+        const isAcive = selected === name;
+
+        return (
+          <button
+            key={index}
+            onClick={() => setSelected(name)}
+            className={clsx(
+              "flex gap-2 items-center px-2 py-1.5 relative rounded-md",
+              isAcive && "pointer-events-none",
+            )}
+            disabled={isAcive}
+          >
+            <Icon width={20} height={20} className="z-10" />{" "}
+            <span className="text-sm z-10 select-none drop-shadow">{name}</span>
+            {isAcive && (
+              <motion.div
+                className="absolute bg-white dark:bg-zinc-900 top-0 left-0 right-0 w-full h-full rounded-md dark:border dark:border-zinc-800/50"
+                layoutId="control"
+                transition={{ type: "spring", stiffness: 200, damping: 30 }}
+              />
+            )}
+          </button>
+        );
+      })}
+    </Section>
+  );
+}
