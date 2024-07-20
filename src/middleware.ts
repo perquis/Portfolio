741d279dd864c07d4ff3a4b59ada5a084768b96f
PerQuis <thetahirpl@gmail.com>
2024-07-20 22:02:46 +0200

    :passport_control: auth(middleware.ts): Added a chain function to gather middlewares list.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,16 @@
+import { chain } from "@nimpl/middleware-chain";
 import createMiddleware from "next-intl/middleware";
 
 import { locales } from "@/config/i18n";
 
-export default createMiddleware({
+const withNextIntl = createMiddleware({
   locales,
+  localeDetection: true,
   defaultLocale: locales[0],
 });
 
+export default chain([withNextIntl]);
+
 export const config = {
-  matcher: ["/", `/(en|pl)/:path*`],
+  matcher: ["/", "/(en|pl)/:path*"],
 };
