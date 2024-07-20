51d566781f84a4e0d937de3f96855d64eeb84078
PerQuis <thetahirpl@gmail.com>
2024-07-20 20:04:43 +0200

    :lipstick: ui(shared): Updated styles for these components.

--- src/shared/ui/card/card.tsx
+++ src/shared/ui/card/card.tsx
@@ -1,7 +1,8 @@
+import clsx from "clsx";
 import Link from "next/link";
 import type { ComponentProps } from "react";
 
-import { Paragraph, Ratio, Section, Title } from "@/shared/ui";
+import { Ratio, Regular, Section, Title } from "@/shared/ui";
 
 type ImageProps = Pick<ComponentProps<typeof Ratio>, "src" | "alt">;
 type NextLinkProps = ComponentProps<typeof Link>;
@@ -10,19 +11,20 @@ type ICard = {
   image: ImageProps;
   title: string;
   description: string;
+  className?: string;
 } & NextLinkProps;
 
-export default function Card({ title, description, image, href }: ICard) {
+export default function Card({ title, description, className, image, href }: ICard) {
   return (
-    <Section className="gap-4">
+    <Section className={clsx("gap-4", className)}>
       <Link href={href}>
         <Ratio resolution="16:9" className="overflow-hidden rounded-xl" {...image} />
       </Link>
-      <Section>
+      <Section className="gap-1">
         <Link href={href}>
           <Title level="h6">{title}</Title>
         </Link>
-        <Paragraph>{description}</Paragraph>
+        <Regular className="text-base">{description}</Regular>
       </Section>
     </Section>
   );
--- src/shared/ui/workflows/workflows.tsx
+++ src/shared/ui/workflows/workflows.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 import { Paragraph, Section, Title } from "@/shared/ui";
 
 interface IWorkflow {
-  items: IItem[];
+  items: Pick<IItem, "title" | "description">[];
 }
 
 export default function Workflows({ items }: IWorkflow) {
@@ -27,11 +27,11 @@ const Item: FC<IItem> = ({ index = 0, title, description, last }) => {
   return (
     <Section>
       <Section className="!flex-row gap-5">
-        <Section className="gap-2 justify-center items-center">
+        <Section className="gap-2 justify-start items-center">
           <div className="w-10 h-10 rounded-full border border-zinc-200 dark:border-zinc-800 flex items-center justify-center text-zinc-950 dark:text-white flex-shrink-0">
             <span>{index + 1}</span>
           </div>
-          <VerticalLine />
+          {!last && <VerticalLine />}
         </Section>
         <Section className="gap-2">
           <Title level="b">{title}</Title>
