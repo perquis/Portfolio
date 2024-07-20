1125ad3edc66f3d0454c204d43382dca2678ed5d
PerQuis <thetahirpl@gmail.com>
2024-07-20 23:04:31 +0200

    :passport_control: auth(next.config.mjs): Added a Content-Security-Policy to next configuration file to secure web application better.

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,36 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/config/i18n.ts");
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    connect-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    block-all-mixed-content;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       config.plugins.push(new ESLintWebpackPlugin());
