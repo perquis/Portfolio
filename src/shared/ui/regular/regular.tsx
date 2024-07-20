0ad57483a23d55257b55e2b5431e40b7d876380d
PerQuis <thetahirpl@gmail.com>
2024-07-20 15:43:22 +0200

    :lipstick: ui(regular.tsx): Updated font size for this component.

--- src/shared/ui/regular/regular.tsx
+++ src/shared/ui/regular/regular.tsx
@@ -5,7 +5,7 @@ type IRegular = ComponentProps<"span">;
 
 export default function Regular({ children, className, ...props }: IRegular) {
   return (
-    <span className={clsx("text-black/50 dark:text-white/50 text-xs", className)} {...props}>
+    <span className={clsx("text-black/50 dark:text-white/50 text-sm", className)} {...props}>
       {children}
     </span>
   );
