5acdfa78c850f661dcac3e0352748fbfddcbcdff
PerQuis <thetahirpl@gmail.com>
2024-07-20 19:12:51 +0200

    :seedling: seed(Created a socials array to homepage.):

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,4 +3,5 @@ export { default as links } from "./links/links";
 export { default as placeholders } from "./placeholders/placeholders";
 export { default as ratios } from "./ratios/ratios";
 export { default as slides } from "./slides/slides";
+export { default as socials } from "./socials/socials";
 export { default as statusActions } from "./status-action/status-action";
---
+++ src/data/socials/socials.ts
@@ -0,0 +1,16 @@
+const socials = [
+  {
+    name: "GitHub",
+    url: "https://github.com/perquis",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/damian-werens",
+  },
+  {
+    name: "X",
+    url: "https://x.com/_perquis",
+  },
+] as const;
+
+export default socials;
