7a9d06a55dd2ffad6a0c8d176967e7b918d12a35
PerQuis <thetahirpl@gmail.com>
2024-07-20 21:27:34 +0200

    :wrench: config(next.config.mjs): Introduced eslint webpack plugin to next configuration file.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
+import ESLintWebpackPlugin from "eslint-webpack-plugin";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/config/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(new ESLintWebpackPlugin());
+    }
+
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
