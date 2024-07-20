a2615df8e13729f51f9eaedf1645cf055baa0c10
PerQuis <thetahirpl@gmail.com>
2024-07-20 13:58:16 +0200

    :recycle: refactor(tab.tsx): Replaced ternary operator to casual conditional statement.

--- src/shared/ui/tab/tab.tsx
+++ src/shared/ui/tab/tab.tsx
@@ -14,7 +14,7 @@ export default function Tab({ children, className, ...props }: ITab) {
     <Link
       className={clsx(
         "text-zinc-500 hover:text-zinc-800 hover:bg-zinc-100 focus-visible:text-zinc-800 focus-visible:bg-zinc-100 dark:hover:bg-zinc-900 dark:focus-visible:bg-zinc-900 rounded-lg dark:text-white/50 dark:hover:text-white/80 dark:focus-visible:text-white/80 px-3 py-2 font-medium transition-colors duration-200 ease-in-out",
-        props.href === pathname ? "text-zinc-800 dark:text-white/80 pointer-events-none" : "",
+        props.href === pathname && "text-zinc-800 dark:text-white/80 pointer-events-none",
         className,
       )}
       {...props}
