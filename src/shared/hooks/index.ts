894d2fd186c04990cd12903b0289b2ccb10949d4
PerQuis <thetahirpl@gmail.com>
2024-07-20 14:58:35 +0200

    :sparkles: feature(use-input.ts): Created a useInput hook.

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,5 +1,6 @@
 export { default as useEventCallback } from "./use-event-callback/use-event-callback";
 export { default as useHideBodyScrollbar } from "./use-hide-body-scrollbar/use-hide-body-scrollbar";
+export { default as useInput } from "./use-input/use-input";
 export { default as useKey } from "./use-key/use-key";
 export { default as useOpen } from "./use-open/use-open";
 export { default as useOutsideOnClick } from "./use-outside-onclick/use-outside-onclick";
