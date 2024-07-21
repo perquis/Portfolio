9130796731882c5a0b2e1c11120bde00a5445fa3
PerQuis <thetahirpl@gmail.com>
2024-07-21 14:10:34 +0200

    :sparkles: feature(providers.tsx): Created the utility functions to create more complex Provider component and improve scalability.

---
+++ src/shared/utils/providers.tsx
@@ -0,0 +1,28 @@
+// https://gist.github.com/phatnguyenuit/68122170e317d13e7148c7563be021b6
+import React from "react";
+
+interface Provider<TProps> {
+  Component: React.ComponentType<React.PropsWithChildren<TProps>>;
+  props?: Omit<TProps, "children">;
+}
+
+export function composeProviders<TProviders extends Array<Provider<any>>>(
+  providers: TProviders,
+): React.ComponentType<React.PropsWithChildren> {
+  const ProviderComponent: React.FunctionComponent<React.PropsWithChildren> = ({ children }) => {
+    const initialJSX = <>{children}</>;
+
+    return providers.reduceRight<JSX.Element>((prevJSX, { Component: CurrentProvider, props = {} }) => {
+      return <CurrentProvider {...props}>{prevJSX}</CurrentProvider>;
+    }, initialJSX);
+  };
+
+  return ProviderComponent;
+}
+
+export function createProvider<TProps>(
+  Component: React.ComponentType<React.PropsWithChildren<TProps>>,
+  props?: Omit<TProps, "children">,
+): Provider<TProps> {
+  return { Component, props };
+}
