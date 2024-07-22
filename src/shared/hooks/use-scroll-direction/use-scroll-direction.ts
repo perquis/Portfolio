08e5eb66d18c9437e157d214d3b721cbb0ef0b5a
PerQuis <thetahirpl@gmail.com>
2024-07-22 12:39:40 +0200

    :adhesive_bandage: fix(navigation.tsx): Disabled hiding the Navigation component when the scrollbar has 0px position.

--- src/shared/hooks/use-scroll-direction/use-scroll-direction.ts
+++ src/shared/hooks/use-scroll-direction/use-scroll-direction.ts
@@ -7,7 +7,7 @@ import { useEventCallback } from "@/shared/hooks";
 type Position = "x" | "y";
 
 export default function useScrollDirection(position: Position = "y") {
-  const [direction, setDirection] = useState(position === "y" ? "down" : "left");
+  const [direction, setDirection] = useState(position === "y" ? "up" : "left");
   const [lastScroll, setLastScroll] = useState(0);
   const [currentPos, setCurrentPos] = useState(0);
 
