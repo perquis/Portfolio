844931baa71623757f60b562c06b1872136e30c6
PerQuis <thetahirpl@gmail.com>
2024-07-23 14:41:53 +0200

    :lock: security(feature-contact-form.tsx): Added a form validation with zod and react-hook-form.

--- src/components/contact-form/feature-contact-form.tsx
+++ src/components/contact-form/feature-contact-form.tsx
@@ -1,31 +1,53 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { Link, usePathname } from "@/next/navigation";
 import * as icons from "@/shared/icons/design";
 import { Button, Form, Input, Paragraph, Regular, Section, StatusDot, Textarea } from "@/shared/ui";
 
+const schema = z.object({
+  name: z.string().min(3).max(32),
+  email: z.string().email().max(64),
+  message: z.string().min(10).max(256),
+});
+
+type Schema = z.infer<typeof schema>;
+
 export const ContactForm = () => {
   const t = useTranslations();
+
   const pathname = usePathname();
-  const isContactPage = pathname === "/contact";
+  const root = pathname === "/contact";
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Schema>({ resolver: zodResolver(schema) });
+
+  const onSubmit = handleSubmit((e) => {
+    console.log(e);
+  });
 
   return (
     <Section className="gap-10">
       <Section className="gap-5">
-        <Section className={clsx("!flex-row", { "justify-between": !isContactPage })}>
+        <Section className={clsx("!flex-row", { "justify-between": !root })}>
           <Regular
             className={clsx(
-              isContactPage ? "font-bold" : "font-semibold",
-              { "text-xl": isContactPage },
-              { "!text-zinc-950 dark:!text-white": isContactPage },
+              root ? "font-bold" : "font-semibold",
+              { "text-xl": root },
+              { "!text-zinc-950 dark:!text-white": root },
             )}
           >
             {t("CONTACT_FORM_TITLE")}
           </Regular>
-          {!isContactPage && (
+          {!root && (
             <Link href="/contact#faq" className="flex gap-2 text-sm font-medium">
               FAQ <icons.ArrowLineRight width={20} height={20} />
             </Link>
@@ -40,13 +62,28 @@ export const ContactForm = () => {
         </Section>
       </Section>
 
-      <Form className="!gap-10">
+      <Form className="!gap-10" onSubmit={onSubmit}>
         <Section className="gap-3">
           <Section className="!flex-row gap-3">
-            <Input name="name" required placeholder="John" />
-            <Input type="email" name="email" required placeholder="jdoe@gmail.com" />
+            <Input
+              placeholder="John"
+              autoComplete="name"
+              error={errors.name?.message}
+              {...register("name", { required: true })}
+            />
+            <Input
+              type="email"
+              autoComplete="email"
+              placeholder="jdoe@gmail.com"
+              error={errors.email?.message}
+              {...register("email", { required: true })}
+            />
           </Section>
-          <Textarea name="message" required placeholder={t("CONTACT_FORM_TEXTAREA_MESSAGE")} />
+          <Textarea
+            error={errors.message?.message}
+            {...register("message", { required: true })}
+            placeholder={t("CONTACT_FORM_TEXTAREA_MESSAGE")}
+          />
         </Section>
         <Button type="submit" mode="simple" size="medium" variants="black" className="!justify-center">
           {t("CONTACT_FORM_BUTTON_SEND")}
