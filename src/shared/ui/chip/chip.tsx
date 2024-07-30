710949782d14ffe443178279610787e7254f870a
PerQuis <thetahirpl@gmail.com>
2024-07-30 17:01:24 +0200

    :lipstick: ui(chip.tsx): Added a Chip component to shared/ui

---
+++ src/shared/ui/chip/chip.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import type { ComponentProps } from "react";
+
+type TChip = ComponentProps<"button">;
+
+export default function Chip({ children, className, ...props }: TChip) {
+  return (
+    <button
+      className={clsx(
+        "rounded-lg bg-zinc-100 px-3 py-1.5 text-sm text-zinc-500 duration-300 ease-in-out hover:bg-zinc-200 hover:text-zinc-700 focus-visible:bg-zinc-200 focus-visible:text-zinc-700 active:scale-95 dark:bg-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-300 dark:focus-visible:bg-zinc-800 dark:focus-visible:text-zinc-300",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -7,6 +7,7 @@ export { default as Breadcrumbs } from "./breadcrumbs/breadcrumbs";
 export { default as Button } from "./button/button";
 export { default as Callout } from "./callout/callout";
 export { default as Card } from "./card/card";
+export { default as Chip } from "./chip/chip";
 export { default as CodeBlock } from "./code-block/code-block";
 export { default as Code } from "./code/code";
 export { default as Container } from "./container/container";
