424b830703e4624b8eb3ccf75e7b185499ac3bd9
PerQuis <thetahirpl@gmail.com>
2024-07-31 19:50:11 +0200

    :globe_with_meridians: i18n(translations): Added new translations to ServicesList component.

--- src/components/services-list/utils-use-items.ts
+++ src/components/services-list/utils-use-items.ts
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import type { ComponentProps } from "react";
 
 import type { List } from "./ui-list";
@@ -5,5 +6,59 @@ import type { List } from "./ui-list";
 type TUseItems = ComponentProps<typeof List>["items"];
 
 export const useItems = (): TUseItems => {
-  return [];
+  const t = useTranslations();
+
+  return [
+    {
+      title: t("HOME_SERVICE_ITEM_LABEL_1"),
+      chips: [
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_1"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_1"),
+        },
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_2"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_2"),
+        },
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_3"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_3"),
+        },
+      ],
+    },
+    {
+      title: t("HOME_SERVICE_ITEM_LABEL_2"),
+      chips: [
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_4"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_4"),
+        },
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_5"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_5"),
+        },
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_6"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_6"),
+        },
+      ],
+    },
+    {
+      title: t("HOME_SERVICE_ITEM_LABEL_3"),
+      chips: [
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_7"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_7"),
+        },
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_8"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_8"),
+        },
+        {
+          label: t("HOME_SERVICE_ITEM_TITLE_9"),
+          description: t("HOME_SERVICE_ITEM_DESCRIPTION_9"),
+        },
+      ],
+    },
+  ];
 };
