f800a72e5976ab06ce3739454bec5ac57551525b
PerQuis <thetahirpl@gmail.com>
2024-07-31 23:49:38 +0200

    :lipstick: ui(video-player.tsx): Created a VideoPlayer component.

---
+++ src/shared/ui/video-player/video-player.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps } from "react";
+
+export default function VideoPlayer(props: ComponentProps<"iframe">) {
+  return (
+    <iframe
+      {...props}
+      className="aspect-video w-full overflow-hidden rounded-xl"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  );
+}
