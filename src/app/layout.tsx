b357c24fc48e0f9e902eab9019e0f5990aab7a87
PerQuis <thetahirpl@gmail.com>
2024-08-02 17:39:04 +0200

    :ambulance: hotfix(layout.tsx): Moved metadata from [locale] to /app to fix problem with metadataBase error message while building web application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
 import type { PropsWithChildren } from "react";
 
+import { locales } from "@/config/i18n";
+
+export const metadata = {
+  metadataBase: new URL(process.env.VERCEL_URL!),
+  alternates: {
+    canonical: "/",
+    languages: {
+      [locales[0]]: `/${locales[0]}`,
+      [locales[1]]: `/${locales[1]}`,
+    },
+  },
+  openGraph: {
+    images: "/static/pages/og-image.png",
+  },
+};
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return children;
 }
