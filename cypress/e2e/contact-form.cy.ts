b3c69b9a29bcd474b583285f35f36996edd4bc10
PerQuis <thetahirpl@gmail.com>
2024-08-04 00:42:18 +0200

    :white_check_mark: tests(contact-form.cy.ts): Updated this test file to check whether the user will see the alert component after he send form.

--- cypress/e2e/contact-form.cy.ts
+++ cypress/e2e/contact-form.cy.ts
@@ -1,20 +1,24 @@
+import en from "../../src/translations/en.json";
+
 describe("<ContactForm />", () => {
   it("tests whether the user can send the emails when the data are correct", () => {
-    cy.visit("http://localhost:3000");
-
     cy.viewport(1280, 720);
+    cy.visit("http://localhost:3000");
 
     cy.get("input[name='name']").click().type("John Doe");
     cy.get("input[name='email']").click().type("jdoe@gmail.com");
     cy.get("textarea[name='message']").click().type("Hello, this is a test message");
     cy.get("input[name='checked']").click();
     cy.get("button[type='submit']").click();
+
+    cy.wait(2000);
+
+    cy.get("#alerts").find("p").should("have.text", en.ALERT_SUCCESSFULLY);
   });
 
   it("tests whether the user can send the emails when the data are incorrect", () => {
-    cy.visit("http://localhost:3000");
-
     cy.viewport(1280, 720);
+    cy.visit("http://localhost:3000");
 
     cy.get("input[name='name']").click().type("Jo");
     cy.get("input[name='checked']").click();
