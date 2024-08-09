43308e9ca6f3f36536f081d8995a3b57acebc7a5
perquis <thetahirpl@gmail.com>
2024-08-09 13:50:10 +0200

    :wheelchair: a11y(checkbox.ts): Added the cursor-pointer class to this component.

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -13,10 +13,10 @@ const Checkbox = forwardRef<HTMLInputElement, TCheckBox>(function Checkbox(props
       <input
         ref={ref}
         type="checkbox"
-        className="peer h-4 w-4 appearance-none rounded border border-white bg-zinc-100 ring-1 ring-zinc-200 checked:!border-indigo-400/50 checked:!bg-indigo-500 checked:!ring-indigo-600 dark:border-zinc-800 dark:bg-zinc-900 dark:ring-zinc-800/50"
+        className="peer h-4 w-4 cursor-pointer appearance-none rounded border border-white bg-zinc-100 ring-1 ring-zinc-200 checked:!border-indigo-400/50 checked:!bg-indigo-500 checked:!ring-indigo-600 dark:border-zinc-800 dark:bg-zinc-900 dark:ring-zinc-800/50"
         {...props}
       />
-      <Checkmark width={12} height={12} className="absolute hidden text-white peer-checked:block" />
+      <Checkmark width={12} height={12} className="pointer-events-none absolute hidden text-white peer-checked:block" />
     </div>
   );
 });
