d4f61b0940cd20a8c6ecce39917f744c759338bb
perquis <thetahirpl@gmail.com>
2024-08-09 14:55:53 +0200

    :sparkles: feature(use-mounted.ts): Created new custom hook that it's called as useMounted.

---
+++ src/shared/hooks/use-mounted/use-mounted.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function useMounted() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+
+    return () => {
+      setMounted(false);
+    };
+  }, []);
+
+  return mounted;
+}
