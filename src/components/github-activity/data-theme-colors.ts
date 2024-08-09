d2a8608f47724c52df685e9bd9ded7225ee58971
perquis <thetahirpl@gmail.com>
2024-08-09 20:07:49 +0200

    :sparkles: feature(feature-github-activity.tsx): Added functionality to select specific year based on range from 2020 to current year.

--- src/components/github-activity/data-theme-colors.ts
+++ src/components/github-activity/data-theme-colors.ts
@@ -2,5 +2,5 @@ import type { ThemeInput } from "react-github-calendar";
 
 export const themeInput: ThemeInput = {
   light: ["#f4f4f5", "#e0e7ff", "#a5b4fc", "#6366f1", "#4338ca"],
-  dark: ["#27272a", "#1e1b4b", "#3730a3", "#4f46e5", "#6366f1"],
+  dark: ["#18181b", "#1e1b4b", "#3730a3", "#4f46e5", "#6366f1"],
 };
