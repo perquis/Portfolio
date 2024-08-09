a475b8424b652db96a6256ccdf236c26e967692a
perquis <thetahirpl@gmail.com>
2024-08-09 13:46:24 +0200

    :label: types(jest.config.ts): Added a type definition to config variable.

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,14 @@
+import type { Config } from "@jest/types";
+
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
-const config = {
+const config: Config.InitialOptions = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   globals: {
     "ts-jest": {
@@ -21,5 +20,4 @@ const config = {
   },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(config);
