87edd13fc2ce50eba65b411d7784b5636da42f2f
perquis <thetahirpl@gmail.com>
2024-08-11 20:33:12 +0200

    :recycle: refactor(chip.tsx): Added new state for this component to use it in more cases.

--- src/shared/ui/chip/chip.tsx
+++ src/shared/ui/chip/chip.tsx
@@ -1,13 +1,19 @@
 import clsx from "clsx";
 import type { ComponentProps } from "react";
 
+interface IChip {
+  isActive?: boolean;
+}
+
 type TChip = ComponentProps<"button">;
 
-export default function Chip({ children, className, ...props }: TChip) {
+export default function Chip({ children, className, isActive, ...props }: TChip & IChip) {
   return (
     <button
       className={clsx(
         "rounded-lg bg-zinc-100 px-3 py-1.5 text-sm text-zinc-500 transition-colors duration-300 ease-in-out hover:bg-zinc-200 hover:text-zinc-700 focus-visible:bg-zinc-200 focus-visible:text-zinc-700 active:scale-95 dark:bg-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-300 dark:focus-visible:bg-zinc-800 dark:focus-visible:text-zinc-300",
+        isActive &&
+          "pointer-events-none !bg-indigo-50 !text-indigo-600 disabled:cursor-default dark:!bg-indigo-950 dark:!text-indigo-400",
         className,
       )}
       {...props}
