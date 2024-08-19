cc66f5d4e0ec65b88fc9df5b091b02fd74a5ca19
perquis <thetahirpl@gmail.com>
2024-08-19 10:27:27 +0200

    :wheelchair: a11y(form): Introduced fixes to Input and Textarea component to resolve problem with hover state while the user moves the mouse over input or textarea.

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -13,21 +13,21 @@ const Input = forwardRef<HTMLInputElement, TInput>(function Input({ className, e
       transition={{ duration: 0.2 }}
       className="w-full"
     >
-      <Section className="gap-1">
-        <label className="flex flex-col gap-2" htmlFor={id}>
+      <Section className="items-start gap-1">
+        <label htmlFor={id}>
           {labelText && <span className="ml-3 text-sm font-medium text-zinc-500">{labelText}</span>}
-
-          <input
-            className={clsx(
-              "w-full rounded-[10px] bg-zinc-100 px-3 py-2.5 text-sm text-zinc-900 placeholder:text-zinc-400 hover:bg-zinc-200 hover:placeholder:text-zinc-500 focus-visible:bg-zinc-200 focus-visible:placeholder:text-zinc-500 dark:bg-zinc-900 dark:text-zinc-100 dark:placeholder:text-zinc-600 dark:hover:bg-zinc-800 dark:hover:placeholder:text-zinc-500 dark:focus-visible:bg-zinc-800 dark:focus-visible:placeholder:text-zinc-500",
-              error && "ring-2 ring-rose-500 focus-visible:!outline-none",
-              className,
-            )}
-            ref={ref}
-            id={id}
-            {...props}
-          />
         </label>
+
+        <input
+          className={clsx(
+            "w-full rounded-[10px] bg-zinc-100 px-3 py-2.5 text-sm text-zinc-900 transition-colors duration-200 placeholder:text-zinc-400 hover:bg-zinc-200 hover:placeholder:text-zinc-500 focus-visible:bg-zinc-200 focus-visible:placeholder:text-zinc-500 dark:bg-zinc-900 dark:text-zinc-100 dark:placeholder:text-zinc-600 dark:hover:bg-zinc-800 dark:hover:placeholder:text-zinc-500 dark:focus-visible:bg-zinc-800 dark:focus-visible:placeholder:text-zinc-500",
+            error && "ring-2 ring-rose-500 focus-visible:!outline-none",
+            className,
+          )}
+          ref={ref}
+          id={id}
+          {...props}
+        />
         {error && <span className="text-sm text-rose-500">{error}</span>}
       </Section>
     </Transition>
--- src/shared/ui/textarea/textarea.tsx
+++ src/shared/ui/textarea/textarea.tsx
@@ -16,21 +16,21 @@ const Textarea = forwardRef<HTMLTextAreaElement, TTextarea>(function Textarea(
       transition={{ duration: 0.2 }}
       className="w-full"
     >
-      <Section className="gap-1">
-        <label className="flex flex-col gap-2" htmlFor={id}>
+      <Section className="items-start gap-1">
+        <label htmlFor={id}>
           {labelText && <span className="ml-3 text-sm font-medium text-zinc-500">{labelText}</span>}
-
-          <textarea
-            className={clsx(
-              "min-h-64 w-full rounded-[10px] bg-zinc-100 px-3 py-2.5 text-sm text-zinc-900 placeholder:text-zinc-400 hover:bg-zinc-200 hover:text-zinc-500 focus-visible:bg-zinc-200 focus-visible:text-zinc-500 dark:bg-zinc-900 dark:text-zinc-100 dark:placeholder:text-zinc-600 dark:hover:bg-zinc-800 dark:hover:placeholder:text-zinc-500 dark:focus-visible:bg-zinc-800 dark:focus-visible:placeholder:text-zinc-500",
-              error && "ring-2 ring-rose-500 focus-visible:!outline-none",
-              className,
-            )}
-            ref={ref}
-            id={id}
-            {...props}
-          />
         </label>
+
+        <textarea
+          className={clsx(
+            "min-h-64 w-full rounded-[10px] bg-zinc-100 px-3 py-2.5 text-sm text-zinc-900 transition-colors duration-200 placeholder:text-zinc-400 hover:bg-zinc-200 hover:text-zinc-500 focus-visible:bg-zinc-200 focus-visible:text-zinc-500 dark:bg-zinc-900 dark:text-zinc-100 dark:placeholder:text-zinc-600 dark:hover:bg-zinc-800 dark:hover:placeholder:text-zinc-500 dark:focus-visible:bg-zinc-800 dark:focus-visible:placeholder:text-zinc-500",
+            error && "ring-2 ring-rose-500 focus-visible:!outline-none",
+            className,
+          )}
+          ref={ref}
+          id={id}
+          {...props}
+        />
         {error && <span className="text-sm text-rose-500">{error}</span>}
       </Section>
     </Transition>
