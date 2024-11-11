0d2aafbd8a09f668a7bb9cfa891b8165106f9206
perquis <thetahirpl@gmail.com>
2024-11-11 22:12:32 +0100

    :bug: bugfix(toggle-theme.tsx): Resolved problem with changing theme mode.

--- src/components/toggle-theme/toggle-theme.tsx
+++ src/components/toggle-theme/toggle-theme.tsx
@@ -9,39 +9,45 @@ import { Moon, Sun } from "@/shared/icons/design";
 import { Section } from "@/shared/ui";
 
 export default function ToggleTheme() {
-  const { theme, setTheme } = useTheme();
+  const { theme, systemTheme, setTheme } = useTheme();
   const mounted = useMounted();
 
-  const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
+  const toggleTheme = () =>
+    setTheme(theme === "system" ? (systemTheme === "dark" ? "light" : "dark") : theme === "dark" ? "light" : "dark");
+
+  const isDarkMode = (theme === "system" && systemTheme === "dark") || theme === "dark",
+    isLightMode = !isDarkMode;
+
+  const isChecked = theme === "system" ? systemTheme === "dark" : theme === "dark";
 
   if (!mounted) return null;
 
   return (
     <Section className="!flex-row items-center gap-1">
       <Sun
-        className={clsx("transition-colors duration-200 ease-in-out", theme === "dark" && "opacity-50")}
+        className={clsx("transition-colors duration-200 ease-in-out", isDarkMode && "opacity-50")}
         width={16}
         height={16}
       />
       <Switch
-        checked={theme === "dark"}
+        checked={isChecked}
         onChange={toggleTheme}
         className={clsx(
           "group relative flex h-5 w-10 cursor-pointer rounded-full border p-1 transition-colors duration-200 ease-in-out focus:outline-none data-[focus]:outline-1 data-[focus]:outline-indigo-600",
-          theme === "light" ? "border-zinc-200" : "border-white/10",
-          theme === "light" ? "bg-white shadow-sm" : "bg-zinc-900",
+          isLightMode ? "border-zinc-200" : "border-white/10",
+          isLightMode ? "bg-white shadow-sm" : "bg-zinc-900",
         )}
       >
         <span
           aria-hidden="true"
           className={clsx(
             "pointer-events-none inline-block size-2.5 translate-x-0 rounded-full shadow-lg ring-0 transition-transform duration-200 ease-in-out group-data-[checked]:translate-x-5",
-            theme === "light" ? "bg-black" : "bg-white",
+            isLightMode ? "bg-black" : "bg-white",
           )}
         />
       </Switch>
       <Moon
-        className={clsx("transition-colors duration-200 ease-in-out", theme === "light" && "opacity-50")}
+        className={clsx("transition-colors duration-200 ease-in-out", isLightMode && "opacity-50")}
         width={16}
         height={16}
       />
