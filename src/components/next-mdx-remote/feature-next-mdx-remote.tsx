0bbc77fe8029b1c08aba78cd16e6873394561537
perquis <thetahirpl@gmail.com>
2024-11-12 15:57:48 +0100

    :recycle: refactor(feature-next-mdx-remote.tsx): Changed the way of importing components to be always up to date.

--- src/components/next-mdx-remote/feature-next-mdx-remote.tsx
+++ src/components/next-mdx-remote/feature-next-mdx-remote.tsx
@@ -3,64 +3,15 @@
 import { MDXRemote, type MDXRemoteProps } from "next-mdx-remote";
 import type { ComponentProps, FC } from "react";
 
-import {
-  Accordion,
-  ArrowLink,
-  Badge,
-  Breadcrumbs,
-  Callout,
-  Card,
-  Code,
-  CodeBlock,
-  CodeSandbox,
-  Container,
-  Details,
-  Divider,
-  Link,
-  MotionPicture,
-  Page,
-  Paragraph,
-  Ratio,
-  Regular,
-  Section,
-  Slider,
-  Technology,
-  Title,
-  VideoPlayer,
-  Workflows,
-} from "@/shared/ui";
+import * as components from "@/shared/ui";
 
-const components: MDXRemoteProps["components"] = {
-  Code,
-  Badge,
-  CodeBlock,
-  Paragraph,
-  Breadcrumbs,
-  Callout,
-  Accordion,
-  Container,
-  Title,
-  Regular,
-  Page,
-  ArrowLink,
-  Slider,
-  Card,
-  Divider,
-  Link,
-  Ratio,
-  Section,
-  Technology,
-  Details,
-  Workflows,
-  MotionPicture,
-  VideoPlayer,
-  CodeSandbox,
-
-  p: (props) => <Paragraph {...props} />,
+const mdxComponents: MDXRemoteProps["components"] = {
+  ...components,
+  p: (props) => <components.Paragraph {...props} />,
 };
 
 type TNextMDXRemote = ComponentProps<typeof MDXRemote>;
 
 export const NextMDXRemote: FC<TNextMDXRemote> = (props) => {
-  return <MDXRemote {...props} components={components} />;
+  return <MDXRemote {...props} components={mdxComponents} />;
 };
