dbbf6f09d4c132e35fe3117cdd50f80fd09506bc
perquis <thetahirpl@gmail.com>
2024-11-12 01:18:11 +0100

    :sparkles: feature(emails): Added react-email to this project to send mails that they look better.

---
+++ src/emails/messages/contact-form.tsx
@@ -0,0 +1,60 @@
+import { Font, Heading, Html, Link, Section, Tailwind, Text } from "@react-email/components";
+
+interface IProps {
+  name: string;
+  email: string;
+  message: string;
+}
+
+interface IContactFormMessageMailProps {
+  email: IProps;
+}
+
+export default function ContactFormMessageMail({ email: { name, email, message } }: IContactFormMessageMailProps) {
+  return (
+    <Tailwind>
+      <Html className="bg-zinc-100">
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+
+        <Section className="mx-auto max-w-screen-sm rounded-xl bg-white p-5 shadow">
+          <Heading className="leading-5 text-black">Hello Damian! 🖐️</Heading>
+
+          <Text className="text-zinc-700">
+            I sent you a mail from your website. Please check it out as soon as possible. You can find my message below:
+          </Text>
+
+          <Text className="text-zinc-700">{message}</Text>
+
+          <Text className="text-zinc-700">
+            <strong>Best regards,</strong>
+            <br />
+            {name}
+          </Text>
+
+          <Text>
+            <Link className="italic text-indigo-600 underline" href={`mailto:${email}`}>
+              {email}
+            </Link>
+          </Text>
+        </Section>
+      </Html>
+    </Tailwind>
+  );
+}
+
+ContactFormMessageMail.PreviewProps = {
+  email: {
+    name: "Daniel",
+    email: "dmartinez@example.com",
+    message: "Hello, I want to talk about a project.",
+  },
+};
---
+++ src/server/actions/sendMail.tsx
@@ -0,0 +1,20 @@
+"use server";
+
+import { render } from "@react-email/components";
+
+import type { Schema } from "@/components";
+import ContactFormMessageMail from "@/emails/messages/contact-form";
+import { transporter } from "@/services/nodemailer";
+import { capitalized } from "@/shared/utils/capitalized";
+
+export async function sendMail({ name, email, message, checked }: Schema) {
+  if (!checked) {
+    throw new Error("Please agree with the policy and privacy.");
+  }
+
+  transporter.sendMail({
+    to: process.env.SMTP_GMAIL_EMAIL,
+    subject: `${capitalized(name)} sent mail for you 📧!`,
+    html: await render(<ContactFormMessageMail email={{ name, email, message }} />),
+  });
+}
