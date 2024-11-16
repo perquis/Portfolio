bc5e385227e8f860eaeaa957b26be13ccbfd2f08
perquis <thetahirpl@gmail.com>
2024-11-16 11:36:49 +0100

    :lipstick: ui(footer): Added LanguageSwitcher component to this area.

--- src/shared/ui/footer/footer.tsx
+++ src/shared/ui/footer/footer.tsx
@@ -1,16 +1,25 @@
-import { useTranslations } from "next-intl";
+import clsx from "clsx";
+import { useLocale, useTranslations } from "next-intl";
 
-import ToggleTheme from "@/components/toggle-theme/toggle-theme";
+import { LanguageSwitcher, ToggleTheme } from "@/components";
 import { Logo, Regular, Section } from "@/shared/ui";
 
 export default function Footer() {
   const t = useTranslations();
+  const locale = useLocale();
 
   return (
     <Section className="!flex-row flex-wrap items-start justify-between gap-5">
       <Logo />
-      <Section className="z-50 items-end gap-2 max-[492px]:!items-start">
+      <Section
+        className={clsx(
+          "z-50 items-end gap-2",
+          locale === "en" && "max-[424px]:!items-start",
+          locale === "pl" && "max-[492px]:!items-start",
+        )}
+      >
         <Regular className="!text-sm">{t("FOOTER_ALL_RIGHTS_RESERVED")}</Regular>
+        <LanguageSwitcher />
         <ToggleTheme />
       </Section>
     </Section>
--- src/shared/ui/tab/tab.tsx
+++ src/shared/ui/tab/tab.tsx
@@ -20,7 +20,7 @@ export default function Tab({ children, className, lock, ...props }: TTab) {
     <Link
       className={clsx(
         "rounded-lg px-3 py-2 font-medium text-zinc-500 transition-colors duration-200 ease-in-out hover:bg-zinc-100 hover:text-zinc-800 focus-visible:bg-zinc-100 focus-visible:text-zinc-800 dark:text-white/50 dark:hover:bg-zinc-900 dark:hover:text-white/80 dark:focus-visible:bg-zinc-900 dark:focus-visible:text-white/80",
-        isActive && "pointer-events-none text-zinc-800 dark:text-white/80",
+        isActive && "pointer-events-none text-zinc-800 dark:!text-white",
         className,
       )}
       tabIndex={lock ? -1 : isActive ? -1 : undefined}
