c85de85c5b322ec324fac2934122c5f1c80c7dd5
perquis <thetahirpl@gmail.com>
2024-11-30 10:03:16 +0100

    :lipstick: ui(code-comparison.tsx): Added stack icons to this component to display programming languages in tab section.

--- src/shared/ui/code-comparison/code-comparison.tsx
+++ src/shared/ui/code-comparison/code-comparison.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { codeToHtml } from "shiki";
 
+import * as stacks from "@/shared/icons/programming-languages";
 import { theme } from "@/shared/themes/theme";
+
+type IconName = keyof typeof stacks;
 
 interface CodeComparisonProps {
   beforeCode: string;
   afterCode: string;
   language: string;
+  icon: IconName;
   filename: string;
   lightTheme: string;
   darkTheme: string;
@@ -22,6 +26,7 @@ export default function CodeComparison({
   beforeCode,
   afterCode,
   language,
+  icon = "Ts",
   filename,
   lightTheme = lightColorScheme, // "light-plus" | "slack-ochin" | "snazzy-light"
   darkTheme = darkColorScheme, // "laserwave" | "houston"
@@ -69,20 +74,29 @@ export default function CodeComparison({
     }
   };
 
+  const icons = useMemo(() => stacks, []);
+  const Icon = icons[icon];
+
   return (
     <div className="mx-auto w-full">
       <div className="relative w-full overflow-hidden rounded-xl border border-border shadow">
         <div className="relative grid md:grid-cols-2 md:divide-x md:divide-border">
           <div>
             <div className="flex items-center bg-accent px-4 py-2 text-sm text-foreground">
-              {filename}
+              <div className="flex items-center gap-2">
+                <Icon width={20} height={20} />
+                {filename}
+              </div>
               <span className="ml-auto">before</span>
             </div>
             {renderCode(beforeCode, highlightedBefore)}
           </div>
           <div>
             <div className="flex items-center bg-accent px-4 py-2 text-sm text-foreground">
-              {filename}
+              <div className="flex items-center gap-2">
+                <Icon width={20} height={20} />
+                {filename}
+              </div>
               <span className="ml-auto">after</span>
             </div>
             {renderCode(afterCode, highlightedAfter)}
