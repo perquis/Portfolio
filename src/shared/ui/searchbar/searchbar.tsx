eba2cab5f7c99b333229618659e10ca3f70122a5
perquis <thetahirpl@gmail.com>
2024-12-02 00:17:41 +0100

    :wheelchair: a11y(searchbar.tsx): Fixed problem with focusing on input while using shift and tab key combinations.

--- src/shared/ui/searchbar/searchbar.tsx
+++ src/shared/ui/searchbar/searchbar.tsx
@@ -23,29 +23,25 @@ export default function SearchBar({ className, ...props }: TSearchBar) {
   }, 500);
 
   const platform = usePlatform();
-
   const specialKey = platform === "Mac OS" ? "⌘" : platform === "Windows" ? "Ctrl" : null;
 
   return (
     <div
       className={clsx(
-        "flex w-full items-center gap-2 rounded-[10px] bg-zinc-100 px-3 pr-2 text-sm text-zinc-900 hover:bg-zinc-200 focus-visible:bg-zinc-200 dark:bg-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-800 dark:focus-visible:bg-zinc-800",
-        className,
+        "flex w-full items-center gap-2 rounded-[10px] bg-zinc-100 px-3 pr-2 text-sm text-zinc-900 !outline-0 hover:bg-zinc-200 focus:!outline-none focus-visible:bg-zinc-200 focus-visible:!outline-none dark:bg-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-800 dark:focus-visible:bg-zinc-800",
         isFocus && "outline outline-offset-[3px] outline-indigo-600/75",
+        className,
       )}
-      tabIndex={0}
-      onClick={focus}
-      onFocus={focus}
-      onBlur={blur}
       onChange={onChange}
     >
       <Search width={20} height={20} className="flex-shrink-0 text-zinc-500" />
       <input
         className="w-full rounded-[10px] bg-transparent py-2.5 !outline-none placeholder:text-zinc-500 focus-visible:!outline-none focus-visible:placeholder:text-zinc-600 dark:hover:placeholder:text-zinc-400 dark:focus-visible:placeholder:text-zinc-400"
         type="search"
+        onFocus={focus}
+        onBlur={blur}
         defaultValue={query ?? ""}
         ref={inputRef}
-        tabIndex={-1}
         {...props}
       />
       <KeyBind className={clsx(!specialKey && "hidden")}>
