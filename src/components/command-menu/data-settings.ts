34538ac1acec3cd0a443430ab6fb6da7b3436003
perquis <thetahirpl@gmail.com>
2024-12-01 15:33:13 +0100

    :bug: bugfix(command-menu): Fixed problem with handling keyboard action while clicking on selected option.

---
+++ src/components/command-menu/data-settings.ts
@@ -0,0 +1,19 @@
+import { AppWindow, Moon, Sun } from "lucide-react";
+
+export const settings = [
+  {
+    Icon: Sun,
+    label: "LIGHT_MODE",
+    selectValue: "light",
+  },
+  {
+    Icon: Moon,
+    label: "DARK_MODE",
+    selectValue: "dark",
+  },
+  {
+    Icon: AppWindow,
+    label: "SYSTEM_MODE",
+    selectValue: "system",
+  },
+];
--- src/components/command-menu/feature-command-menu.tsx
+++ src/components/command-menu/feature-command-menu.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { Moon, Sun, SunMoon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { useTheme } from "next-themes";
 import { useEffect } from "react";
 
 import { links } from "@/data";
-import { Link } from "@/libs/next-intl";
+import { useRouter } from "@/libs/next-intl";
 import { useOpen } from "@/shared/hooks";
 import {
   CommandDialog,
@@ -17,6 +16,8 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/shared/ui/command/command";
+
+import { settings } from "./data-settings";
 
 export function CommandMenu() {
   const [isOpen, [, , toggle]] = useOpen();
@@ -36,6 +37,8 @@ export function CommandMenu() {
     return () => document.removeEventListener("keydown", down);
   }, [toggle]);
 
+  const { push } = useRouter();
+
   return (
     <CommandDialog open={isOpen} onOpenChange={toggle}>
       <CommandInput placeholder={t("BLOG_SEARCHBAR_PLACEHOLDER")} />
@@ -43,35 +46,26 @@ export function CommandMenu() {
         <CommandEmpty>{t("NO_RESULTS")}</CommandEmpty>
         <CommandGroup heading={t("SUGGESTIONS")}>
           {links.map(({ Icon, href, label }) => (
-            <CommandItem key={label} asChild>
-              <Link href={href} className="flex w-full items-center gap-4">
+            <CommandItem key={label} asChild onSelect={() => push(href)}>
+              <div className="flex w-full items-center gap-4">
                 <Icon />
                 {/* @ts-expect-error */}
                 {t(`NAVIGATION_${label.toUpperCase()}`)}
-              </Link>
+              </div>
             </CommandItem>
           ))}
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading={t("SETTINGS")}>
-          <CommandItem className="w-full" asChild>
-            <button onClick={() => setTheme("light")}>
-              <Sun />
-              <span className="ml-2">{t("LIGHT_MODE")}</span>
-            </button>
-          </CommandItem>
-          <CommandItem className="w-full" asChild>
-            <button onClick={() => setTheme("dark")}>
-              <Moon />
-              <span className="ml-2">{t("DARK_MODE")}</span>
-            </button>
-          </CommandItem>
-          <CommandItem className="w-full" asChild>
-            <button onClick={() => setTheme("system")}>
-              <SunMoon />
-              <span className="ml-2">{t("SYSTEM_MODE")}</span>
-            </button>
-          </CommandItem>
+          {settings.map(({ Icon, label, selectValue }) => (
+            <CommandItem onSelect={() => setTheme(selectValue)} key={label} asChild>
+              <button className="flex w-full items-center gap-4">
+                <Icon />
+                {/* @ts-expect-error */}
+                {t(label)}
+              </button>
+            </CommandItem>
+          ))}
         </CommandGroup>
       </CommandList>
     </CommandDialog>
