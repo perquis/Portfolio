1c34d4b7869a7a85f856b53732200ec894d9e752
perquis <thetahirpl@gmail.com>
2024-12-02 23:51:47 +0100

    :recycle: refactor(use-is-mobile.ts): Modified syntax to look more readable than it was.

--- src/shared/hooks/use-is-mobile/use-is-mobile.ts
+++ src/shared/hooks/use-is-mobile/use-is-mobile.ts
@@ -4,7 +4,10 @@ import { usePlatform } from "@/shared/hooks";
 
 export default function useIsMobile() {
   const { platform } = usePlatform();
-  const isMobile = useMemo(() => platform && (platform === "iOS" || platform === "Android"), [platform]);
+  const isMobile = useMemo(() => {
+    if (!platform) return false;
+    return platform === "iOS" || platform === "Android";
+  }, [platform]);
 
   return { isMobile };
 }
