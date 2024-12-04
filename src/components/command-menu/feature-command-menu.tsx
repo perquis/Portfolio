aa5e1ced8106c035e2cbd5e4b41363ed87763536
perquis <thetahirpl@gmail.com>
2024-12-04 22:23:34 +0100

    :sparkles: feature(command-menu): Added new functionality to search for projects and posts by using CommandMenu component.

--- src/components/command-menu/feature-command-menu.tsx
+++ src/components/command-menu/feature-command-menu.tsx
@@ -2,12 +2,14 @@
 
 import { useLocale, useTranslations } from "next-intl";
 import { useTheme } from "next-themes";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { locales } from "@/config/i18n";
 import { links } from "@/data";
+import type { TMetadata } from "@/interfaces/markdown";
 import { usePathname, useRouter } from "@/libs/next-intl";
-import { useOpen } from "@/shared/hooks";
+import { getArticles } from "@/server/actions/get-articles";
+import { useDebounce, useEventCallback, useOpen } from "@/shared/hooks";
 import {
   CommandDialog,
   CommandEmpty,
@@ -17,9 +19,12 @@ import {
   CommandList,
   CommandSeparator,
 } from "@/shared/ui/command/command";
+import { isEmptyArray } from "@/shared/utils";
 import { getLanguageNames } from "@/shared/utils/get-language-names";
 
 import { settings } from "./data-settings";
+
+type Article = Pick<TMetadata, "title" | "slug">;
 
 export function CommandMenu() {
   const { setTheme } = useTheme();
@@ -29,23 +34,42 @@ export function CommandMenu() {
   const [isOpen, [, , toggle]] = useOpen();
   const t = useTranslations();
 
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault();
-        toggle();
-      }
-    };
+  const [posts, setPosts] = useState<Article[]>([]);
+  const [projects, setProjects] = useState<Article[]>([]);
 
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-  }, [toggle]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const down = (e: KeyboardEvent) => {
+    if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+      e.preventDefault();
+      toggle();
+    }
+  };
 
   const changeLanguage = (locale: string) => push(pathname, { locale });
+  const onValueChange = (search: string) => setSearchQuery(search);
+
+  useEffect(() => {
+    if (searchQuery.length > 0) return;
+
+    setPosts([]);
+    setProjects([]);
+  }, [searchQuery]);
+
+  useDebounce(async () => {
+    if (searchQuery.length === 0) return;
+
+    const data = await getArticles(locale, searchQuery);
+
+    setPosts(data.posts);
+    setProjects(data.projects);
+  }, 500);
+
+  useEventCallback({ eventName: "keydown", callback: down });
 
   return (
     <CommandDialog open={isOpen} onOpenChange={toggle}>
-      <CommandInput placeholder={t("COMMAND_SEARCH_PLACEHOLDER")} />
+      <CommandInput placeholder={t("COMMAND_SEARCH_PLACEHOLDER")} onValueChange={onValueChange} />
       <CommandList className="py-1">
         <CommandEmpty>{t("NO_RESULTS")}</CommandEmpty>
         <CommandGroup heading={t("SUGGESTIONS")}>
@@ -81,6 +105,24 @@ export function CommandMenu() {
             </CommandItem>
           ))}
         </CommandGroup>
+        {!isEmptyArray(posts) && (
+          <CommandGroup heading={t("POSTS")}>
+            {posts.map(({ title, slug }) => (
+              <CommandItem key={title} onSelect={() => push(`/blog/${slug}`)}>
+                {title}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        )}
+        {!isEmptyArray(projects) && (
+          <CommandGroup heading={t("PROJECTS")}>
+            {projects.map(({ title, slug }) => (
+              <CommandItem key={title} onSelect={() => push(`/portfolio/${slug}`)}>
+                {title}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        )}
       </CommandList>
     </CommandDialog>
   );
