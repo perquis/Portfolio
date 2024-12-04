3720192853fc2ccdd85082bc62d3a3a017fd4b31
perquis <thetahirpl@gmail.com>
2024-12-04 01:30:07 +0100

    :sparkles: feature(libs/api): Created withErrorHandling function to make refactoring for http methods.

--- src/app/(frontend)/api/op/route.ts
+++ src/app/(frontend)/api/op/route.ts
@@ -1,27 +1,17 @@
 import type { Locale } from "@/interfaces/i18n";
 import type { Location } from "@/interfaces/markdown";
-import { headers } from "@/libs/api";
+import { headers, withErrorHandling } from "@/libs/api";
 import { getSourcesSinceMdxFiles } from "@/shared/utils";
 
-export const GET = async (request: Request) => {
-  try {
-    const url = new URL(request.url);
-    const location = url.searchParams.get("location") as Location,
-      slug = url.searchParams.get("slug")!,
-      locale = url.searchParams.get("locale") as Locale;
+export const GET = withErrorHandling(async (request: Request) => {
+  const url = new URL(request.url);
+  const location = url.searchParams.get("location") as Location,
+    slug = url.searchParams.get("slug")!,
+    locale = url.searchParams.get("locale") as Locale;
 
-    if (!location || !slug || !locale)
-      return new Response(JSON.stringify({ success: false, message: "Invalid parameters" }), { headers, status: 400 });
+  if (!location || !slug || !locale)
+    return new Response(JSON.stringify({ success: false, message: "Invalid parameters" }), { headers, status: 400 });
 
-    const { frontmatter } = await getSourcesSinceMdxFiles(location, slug, locale);
-    return new Response(JSON.stringify(frontmatter.open_graph_img), { headers });
-  } catch (error) {
-    if (error instanceof Error)
-      return new Response(JSON.stringify({ success: false, message: error.message }), { headers, status: 400 });
-
-    return new Response(JSON.stringify({ success: false, message: "Something went wrong..." }), {
-      headers,
-      status: 500,
-    });
-  }
-};
+  const { frontmatter } = await getSourcesSinceMdxFiles(location, slug, locale);
+  return new Response(JSON.stringify(frontmatter.open_graph_img), { headers });
+});
--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,3 +1,31 @@
 export const headers = {
   "content-type": "application/json",
 };
+
+export const withErrorHandling =
+  <T, U>(fn: (request: T) => Promise<U>) =>
+  async (request: T) => {
+    try {
+      return await fn(request);
+    } catch (error) {
+      if (error instanceof Error)
+        return new Response(
+          JSON.stringify({
+            success: false,
+            message: error.message,
+          }),
+          { headers, status: 400 },
+        );
+
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "Something went wrong...",
+        }),
+        {
+          headers,
+          status: 500,
+        },
+      );
+    }
+  };
