a5f4b661d2d2097d688ebeadd3b3e78489a7019a
perquis <thetahirpl@gmail.com>
2024-12-04 22:55:34 +0100

    :green_heart: fixci(src): Introduced different fixes to resolve problem with building app.

--- src/shared/hooks/use-debounce/use-debounce.ts
+++ src/shared/hooks/use-debounce/use-debounce.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const useDebounce = async (delayedFn: () => void, time = 500) => {
+const useDebounce = (delayedFn: () => void, time = 500) => {
   useEffect(() => {
     const timeoutId = setTimeout(delayedFn, time);
     return () => clearTimeout(timeoutId);
