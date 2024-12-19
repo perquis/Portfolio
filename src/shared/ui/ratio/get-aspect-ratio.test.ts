a1a45f4d51aaa320ffb20aca03f853e0dde35bce
perquis <thetahirpl@gmail.com>
2024-12-19 09:00:40 +0100

    :white_check_mark: tests(get-aspect-ratio.test.ts): Updated this test because it had bad name.

--- src/shared/ui/ratio/get-aspect-ratio.test.ts
+++ src/shared/ui/ratio/get-aspect-ratio.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "@jest/globals";
 import { getAspectRatio } from "@/shared/ui/ratio/get-aspect-ratio";
 
 describe("getAspectRatio", () => {
-  it("should return 1 / 1", () => {
+  it("should return aspect ratio: 1 / 1", () => {
     const result = getAspectRatio("1:1");
     expect(result).toEqual({ aspectRatio: "1 / 1" });
   });
