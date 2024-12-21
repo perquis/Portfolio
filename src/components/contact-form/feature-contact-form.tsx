44c719ca02357bcc08f90fae688c9f2554b56749
perquis <thetahirpl@gmail.com>
2024-12-21 12:34:35 +0100

    :ambulance: hotfix(contact-form): Fixed problem with hiding ContactForm component on production mode.

--- src/components/contact-form/feature-contact-form.tsx
+++ src/components/contact-form/feature-contact-form.tsx
@@ -1,5 +1,7 @@
 import { ClientContactForm } from "./ui-client-contact-form";
 
 export const ContactForm = () => {
+  if (process.env.NEXT_PUBLIC_CONTACT_FORM === "true") return null;
+
   return <ClientContactForm />;
 };
--- src/shared/ui/layout/layout.tsx
+++ src/shared/ui/layout/layout.tsx
@@ -2,10 +2,12 @@ import type { PropsWithChildren } from "react";
 import { Children, Fragment, isValidElement } from "react";
 
 import { Container, Divider, Footer, Section } from "@/shared/ui";
-import { snake_case } from "@/shared/utils";
 
 export default function Layout({ children }: PropsWithChildren) {
-  const extractedChildren = Children.toArray(children);
+  const extractedChildren = Children.toArray(children).filter(
+    (Child) => isValidElement(Child) && typeof Child.type === "function" && (Child as JSX.Element).type(Child.props),
+  );
+
   const pageComponents = [
     ...extractedChildren,
     <Container key={crypto.randomUUID()}>
@@ -15,24 +17,14 @@ export default function Layout({ children }: PropsWithChildren) {
 
   return (
     <Section className="gap-10">
-      {pageComponents
-        .filter((Child) => {
-          const Component = Child as { type: { name: string } };
-          const sharedComponentIdentifier = snake_case(Component.type.name);
-          const runtimeEnvKey =
-            process.env[sharedComponentIdentifier?.toUpperCase()] ??
-            process.env[`NEXT_PUBLIC_${sharedComponentIdentifier?.toUpperCase()}`];
-
-          return runtimeEnvKey !== "true";
-        })
-        .map((Child) => {
-          return (
-            <Fragment key={crypto.randomUUID()}>
-              {isValidElement(Child) && Child}
-              {Child !== pageComponents.at(-1) ? <Divider /> : null}
-            </Fragment>
-          );
-        })}
+      {pageComponents.map((Child) => {
+        return (
+          <Fragment key={crypto.randomUUID()}>
+            {isValidElement(Child) && Child}
+            {Child !== pageComponents.at(-1) ? <Divider /> : null}
+          </Fragment>
+        );
+      })}
     </Section>
   );
 }
