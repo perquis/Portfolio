33b773a857b160566608a45328c626b0cbba066c
perquis <thetahirpl@gmail.com>
2024-12-21 20:23:50 +0100

    :globe_with_meridians: i18n(hamburger-menu.tsx): Added translations for navigation links.

---
+++ src/shared/ui/hamburger-menu/anime-rotation.tsx
@@ -0,0 +1,8 @@
+import type { Variants } from "framer-motion";
+
+export const rotation: Variants = {
+  topStart: { translateY: 6, rotate: 0 },
+  topEnd: { translateY: 1, rotate: 45 },
+  bottomStart: { translateY: -2, rotate: 0 },
+  bottomEnd: { translateY: 1, rotate: -45 },
+};
--- src/shared/ui/hamburger-menu/hamburger-menu.tsx
+++ src/shared/ui/hamburger-menu/hamburger-menu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { MotionProps } from "framer-motion";
+import { useTranslations } from "next-intl";
 import { type ComponentProps, type FC, useRef } from "react";
 import ReactFocusLock from "react-focus-lock";
 
@@ -8,28 +9,35 @@ import { useHideBodyScrollbar, useKey, useOpen, useOutsideOnClick } from "@/shar
 import type { Breadcrumbs } from "@/shared/ui";
 import { Section, Tab, Transition } from "@/shared/ui";
 
+import { rotation } from "./anime-rotation";
+
 type THamburgerMenu = ComponentProps<typeof Breadcrumbs>;
+type TLink = "NAVIGATION_ABOUT" | "NAVIGATION_PORTFOLIO" | "NAVIGATION_BLOG" | "NAVIGATION_CONTACT";
 
 export default function HamburgerMenu({ links }: THamburgerMenu) {
   const ref = useRef<HTMLButtonElement>(null);
+  const t = useTranslations();
 
   const [isOpen, [, close, toggle]] = useOpen();
   useOutsideOnClick(ref, close);
   useHideBodyScrollbar(isOpen);
   useKey("Escape", close);
 
+  const topAnimationState = isOpen ? "topEnd" : "topStart";
+  const bottomAnimationState = isOpen ? "bottomEnd" : "bottomStart";
+
   return (
     <Section className="relative w-fit sm:hidden">
       <ReactFocusLock disabled={!isOpen}>
-        <button className="relative h-8 w-8 items-center justify-center rounded-lg" onClick={toggle} ref={ref}>
-          <Line animate={isOpen ? { translateY: 0, rotate: 45 } : { translateY: 6, rotate: 0 }} />
-          <Line animate={isOpen ? { translateY: 0, rotate: -45 } : { translateY: -6, rotate: 0 }} />
+        <button className="relative h-6 w-6 items-center justify-center rounded-lg" onClick={toggle} ref={ref}>
+          <Line variants={rotation} initial="topStart" animate={topAnimationState} />
+          <Line variants={rotation} initial="bottomStart" animate={bottomAnimationState} />
         </button>
         {isOpen && (
-          <Section className="absolute right-0 top-12 w-64 rounded-xl border border-zinc-300 bg-white p-1 shadow-md dark:border-zinc-800 dark:bg-zinc-950">
+          <Section className="absolute right-0 top-12 w-48 rounded-xl border border-zinc-300 bg-white p-1 shadow-md dark:border-zinc-800 dark:bg-zinc-950">
             {links.map(({ Icon, href, label }, index) => (
-              <Tab key={index} className="flex w-full gap-3" href={href} onClick={close}>
-                <Icon className="size-5" /> {label}
+              <Tab key={index} className="flex w-full gap-3 !text-xs" href={href} onClick={close}>
+                <Icon className="size-4" /> {t(`NAVIGATION_${label.toUpperCase()}` as TLink)}
               </Tab>
             ))}
           </Section>
