a7cf5364fcf8b1fe257e3f497fa2dd467f62593b
perquis <thetahirpl@gmail.com>
2025-01-03 23:27:03 +0100

    :ambulance: hotfix(slider): Resolved problem with zooming image while clicking.

--- src/components/services-list/feature-services-list.tsx
+++ src/components/services-list/feature-services-list.tsx
@@ -1,4 +1,4 @@
-import { Header, Section } from "@/shared/ui";
+import { Header, Motion, Section } from "@/shared/ui";
 
 import { List } from "./ui-list";
 import { useItems } from "./utils-use-items";
@@ -7,9 +7,11 @@ export const ServicesList = () => {
   const items = useItems();
 
   return (
-    <Section className="gap-5">
-      <Header heading="HOME_SERVICES_TITLE" description="HOME_SERVICES_DESCRIPTION" />
-      <List items={items} />
-    </Section>
+    <Motion layout>
+      <Section className="gap-5">
+        <Header heading="HOME_SERVICES_TITLE" description="HOME_SERVICES_DESCRIPTION" />
+        <List items={items} />
+      </Section>
+    </Motion>
   );
 };
--- src/components/social-links-list/feature-social-links-list.tsx
+++ src/components/social-links-list/feature-social-links-list.tsx
@@ -1,21 +1,29 @@
+import { useMemo } from "react";
+
 import { socials } from "@/data";
-import { Details, Header, Section } from "@/shared/ui";
+import { Details, Header, Motion, Section } from "@/shared/ui";
 
 export const SocialLinksList = () => {
-  const items = socials.map(
-    (social) =>
-      ({
-        label: String(social.name),
-        content: social.url.split("/").at(-1) as string,
-        type: "link",
-        href: String(social.url),
-      }) as const,
+  const items = useMemo(
+    () =>
+      socials.map(
+        (social) =>
+          ({
+            label: String(social.name),
+            content: social.url.split("/").at(-1) as string,
+            type: "link",
+            href: String(social.url),
+          }) as const,
+      ),
+    [],
   );
 
   return (
-    <Section className="gap-5">
-      <Header heading="CONTACT_SOCIALS_TITLE" />
-      <Details items={items} />
-    </Section>
+    <Motion layout>
+      <Section className="gap-5">
+        <Header heading="CONTACT_SOCIALS_TITLE" />
+        <Details items={items} />
+      </Section>
+    </Motion>
   );
 };
