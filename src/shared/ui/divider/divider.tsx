25f9032ed7b2907073d51a995c4ae41b465376eb
perquis <thetahirpl@gmail.com>
2025-01-04 01:20:46 +0100

    :iphone: responsive(divider): Created temporary solution for this component.

--- src/shared/ui/divider/divider.tsx
+++ src/shared/ui/divider/divider.tsx
@@ -2,15 +2,38 @@
 
 import clsx from "clsx";
 import { type MotionProps, motion } from "framer-motion";
+import { useState } from "react";
+
+import { useEventCallback } from "@/shared/hooks";
 
 type TDivider = MotionProps & { className?: string };
 
 export default function Divider({ className, ...props }: TDivider) {
+  const [innerWidth, setInnerWidth] = useState(0);
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    if (width !== innerWidth) {
+      setInnerWidth(width);
+    }
+  };
+
+  useEventCallback({ callback: handleResize, eventName: "resize" });
+
+  const dots = Array.from({ length: 32 }, (_, index) => (
+    <div
+      key={crypto.randomUUID()}
+      className={clsx(
+        "h-[1px] flex-grow rounded-sm bg-zinc-300 dark:bg-zinc-700",
+        innerWidth >= 400 && innerWidth < 640 && index > 20 && "hidden sm:block",
+        innerWidth <= 400 && index > 12 && "!hidden sm:!block",
+      )}
+    ></div>
+  ));
+
   return (
-    <motion.div layout="position" className={clsx("text-zinc-200 dark:text-zinc-800", className)} {...props}>
-      <svg width="100%" height="1" viewBox="0 0 640 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <line x1="0.5" y1="0.5" x2="639.5" y2="0.5" stroke="currentColor" strokeLinecap="round" strokeDasharray="8" />
-      </svg>
+    <motion.div layout="position" className={clsx("flex gap-2", className)} {...props}>
+      {dots}
     </motion.div>
   );
 }
